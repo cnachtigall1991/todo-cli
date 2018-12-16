d6ba34a328f38f1e9c7f70d578cbe8a978deb4f7
[cnachtigall1991] <[cw.nachtigall@gmail.com]>
2018-12-16 21:23:50 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ struct TodoItem {
     completed: char
 }
 
+impl TodoItem {
+    fn new(name: String) -> TodoItem {
+        return TodoItem{
+            name: name,
+            completed: ' '
+        };
+    }
+}
+
 fn main() {
     let arguments: Vec<String> = env::args().collect();
     let command = arguments[1].clone();
-    let todo_item = TodoItem{
-        name: "Say hi to CJ".to_string(),
-        completed: ' '
-    };
-    let todo_list = vec![todo_item];
+    let todo_item_1 = TodoItem::new("Say hi to CJ".to_string());
+    let todo_item_2 = TodoItem::new("Do something with Rust".to_string());
+    let todo_list = vec![todo_item_1, todo_item_2];
 
     if command == "get" {
         for item in todo_list {
